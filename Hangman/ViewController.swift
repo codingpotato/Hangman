631f9900fcbd44b5181d133774a4eae02f7e0842
codingpotato <codingpotato@gmail.com>
2016-04-24 08:00:14 +0800

    Create xcode project

---
+++ Hangman/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hangman
+//
+//  Created by wangyw on 4/24/16.
+//  Copyright © 2016 codingpotato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
