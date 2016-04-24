17c1a8471dbbbfe837a0c5cf6d5226af61e09896
codingpotato <codingpotato@gmail.com>
2016-04-24 08:18:23 +0800

    Create UI for main view controller

--- Hangman/ViewController.swift
+++ Hangman/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var triesLabel: UILabel!
+    @IBOutlet weak var lengthLabel: UILabel!
+    @IBOutlet weak var usedLabel: UILabel!
+    @IBOutlet weak var wordLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func keyPressed(button: UIButton) {
+    }
+    
 }
 
